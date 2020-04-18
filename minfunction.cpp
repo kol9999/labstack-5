c294c5e8713aeb8dd2dc2f6ecf8ebc005ca56f4e
takbir miah <62444527+kol9999@users.noreply.github.com>
2020-04-18 13:42:51 +0300

    Add files via upload

---
+++ minfunction.cpp
@@ -0,0 +1,25 @@
+// minimum function
+#include <iostream>
+using namespace std;
+template <class any>
+any findMin(any a, any b) {
+    return (a<b?a:b);
+}
+int main(){
+    // Function template test
+    char x = '@', y = '$';
+    std::cout << findMin(x, y) << std::endl;
+
+    double q = 64.2289 , w = 54.1723;
+    std::cout << findMin(q, w) << std::endl;
+
+    std::cout << std::endl;
+    return 0;
+}
+
+
+
+
+
+
+
